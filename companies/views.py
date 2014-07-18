b62a341cf97f49aee7ab7cdc7124718574f3ae9d
Srinivas Adivi <ssrinivas@zeomega.com>
2014-07-18 17:42:31 +0530

    Added graph view fixture for stocks.
    Added static settings
    Added js libraries.
    w.r.t that modified companies views.py

--- companies/views.py
+++ companies/views.py
@@ -2,9 +2,12 @@
 Views for the companies app.
 """
 import json
+
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.http import HttpResponseNotAllowed
 from django.shortcuts import render_to_response
+from django.core import serializers
+
 from companies.models import Company
 from stocks.models import Stock 
 
@@ -41,18 +44,28 @@ def company_stock_details(request, code, startD, endD=None):
         startD = startD[-4:]+'-'+startD[2:4]+'-'+startD[:2]
         if endD:
             endD = endD[-4:]+'-'+endD[2:4]+'-'+endD[:2]
-            stocks = Stock.objects.filter(company=code, date__range=(startD, endD)).order_by('date')[::-1]
+            stocks = Stock.objects.filter(company=code, date__range=(startD, endD)).order_by('date')
+            company_name = json.dumps(stocks[0].company.name.strip())
         else:
             stocks = Stock.objects.filter(company=code, date=startD)
-            
+            company_name = json.dumps(stocks[0].company.name.strip())
     except Stock.DoesNotExist:
         return HttpResponse(status=404)
+    except IndexError:
+        company_name = '' 
 
     if request.method == 'GET':
-        # GET returns a stock object with given id
-        return render_to_response('stocks.json', {'stocks': stocks},
-                                  content_type='application/json')
         
+        # GET returns a stock object with given id
+        if endD:
+            stocks_as_json = serializers.serialize('json', stocks)
+            return render_to_response('company_graph.html', 
+						{"company_name": company_name, "data": stocks_as_json},
+						content_type='application/xhtml+xml')
+        else:
+            return render_to_response('stocks.json', {'stocks': stocks},
+                                  content_type='application/json')
+
     # Notify client of supported methods
     return HttpResponseNotAllowed(['GET'])
 
--- stockmarket/settings.py
+++ stockmarket/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,4 +82,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(PROJECT_ROOT, 'static/')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, "static"),
+)
