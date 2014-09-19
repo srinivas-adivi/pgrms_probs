b0f829934235b25959a323df86c47bc1b12931a8
srinivas.adivi <srinivas.adivi@gmail.com>
2014-09-19 11:17:26 +0530

    Modified 'docstrings' in problems01to09

--- project-eulers/problems01to09.py
+++ project-eulers/problems01to09.py
@@ -1,7 +1,7 @@
 import lib
 
 def problem1(num):
-    """ return sum of all multiplyer of 3 or 5 below num
+    """ Return sum of all multiplyer of 3 or 5 below num.
     >>> problem1(10)
     23
     >>> problem1(1000)
@@ -10,8 +10,8 @@ def problem1(num):
     return sum([x for x in range(num)if x%3==0 or x%5==0])
 
 def problem2(num):
-    """ return the sum of all the even-valued terms in the sequence which do 
-    not exceed num.
+    """ Return the sum of all the even-valued terms in the sequence 
+    which do not exceed num.
     >>> problem2(100)
     188
     >>> problem2(4000000)
@@ -22,7 +22,7 @@ def problem2(num):
     return sum(fib[1::3])
 
 def problem3(num):
-    """
+    """ Largest prime factor.
     >>> problem3(13195)
     29
     >>> problem3(600851475143)
@@ -34,8 +34,8 @@ def problem3(num):
     return primes[-1]
 
 def problem4(start, end):
-    """ return largest palindrome made from the product of numbers between 
-    start and end
+    """ Return largest palindrome made from the product of numbers 
+    between start and end.
     >>> problem4(10, 100)
     9009
     >>> problem4(100, 1000)
@@ -50,7 +50,7 @@ def problem4(start, end):
     return result
 
 def problem5(start, end):
-    """ return least common multiplyer of all numbers between start and 
+    """ Return least common multiplyer of all numbers between start and 
     end(include)
     >>> problem5(2, 5)
     60
@@ -66,8 +66,8 @@ def problem5(start, end):
     return result
 
 def problem6(n):
-    """ return the difference between square of sum of first n natural and sum
-    of squares of first n natural numbers
+    """ Return the difference between square of sum of first n natural and sum
+    of squares of first n natural numbers.
     >>> problem6(10)
     2640
     >>> problem6(100)
@@ -78,7 +78,7 @@ def problem6(n):
     return (sum_numbers*sum_numbers) - sum_squres
 
 def problem7(n):
-    """ return nth prime
+    """ Return nth prime.
     >>> problem7(1)
     2
     >>> problem7(10001)
@@ -91,7 +91,7 @@ def problem7(n):
     return num
 
 def problem8(filename):
-    """ return Find the greatest product of five consecutive digits in given
+    """ Return the greatest product of five consecutive digits in given
     file which contains the 1000-digit number.
     >>> problem8('problem08.txt')
     40824
@@ -101,7 +101,7 @@ def problem8(filename):
     return lib.greatest_product_row(matrix, n)
 
 def problem9(n):
-    """
+    """ Special Pythagorean triplet.
     >>> problem9(1000)
     31875000
     """
